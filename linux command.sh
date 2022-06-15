8164278dc34e34706d0f4ceec3408b445a9577f5
sylves <aminsylves76.gmail.com>
2022-06-15 18:37:08 -0400

    editing commands

---
+++ linux command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+## Author:Sylvestre
+#date june 15
+#Scripts for linus commands
+echo "checking my memory size please wait ..."
+free -m
+echo "checking my the number of cpu ..."
+lscpu
